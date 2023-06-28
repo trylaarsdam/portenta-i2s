68f5bcdaad89bf704309566ed7b380ae18e7e11e
Todd Rylaarsdam <todd@toddr.org>
2023-06-28 13:44:48 -0500

    recording functions

--- src/portenta-i2s.cpp
+++ src/portenta-i2s.cpp
@@ -39,15 +39,7 @@ void PortentaI2S::begin(void)
 }
 
 void PortentaI2S::play(uint16_t* buffer, int bufferSize)
-{
-	/*
-	if(HAL_I2S_Transmit(&hi2s2, buffer, bufferSize, HAL_MAX_DELAY) != HAL_OK)
-	{
-		Serial.println("HAL I2S Transmit Error");
-	}
-	delay(bufferSize / audioFreq * 1000);
-	*/
-	
+{	
 	uint16_t numBuffers = bufferSize / I2S_BUFFER_SIZE;
 	if(bufferSize % I2S_BUFFER_SIZE != 0)
 	{
@@ -83,10 +75,96 @@ void PortentaI2S::play(uint16_t* buffer, int bufferSize)
 
 void PortentaI2S::record(uint32_t* buffer, uint32_t size)
 {
+	uint32_t counter = size, dummy;
 
+	// Check if I2S is enabled
+	if((hi2s2.Instance->CR1 & SPI_CR1_SPE) == SPI_CR1_SPE)
+	{
+		// Disable it
+		__HAL_I2S_DISABLE(&hi2s2);
+	}
+
+	// Clear I2S flags
+	__HAL_I2S_CLEAR_OVRFLAG(&hi2s2);
+	__HAL_I2S_CLEAR_TIFREFLAG(&hi2s2);
+	__HAL_I2S_CLEAR_UDRFLAG(&hi2s2);
+
+	// Check for dummy data in RXDR
+	if(__HAL_I2S_GET_FLAG(&hi2s2, I2S_FLAG_RXP) == SET)
+	{
+		dummy = hi2s2.Instance->RXDR;
+	}
+
+	// Enable I2S
+	__HAL_I2S_ENABLE(&hi2s2);
+
+	// Start data transfer
+	SET_BIT(hi2s2.Instance->CR1, SPI_CR1_CSTART);
+
+	while(counter > 0)
+	{
+		// wait for RXP flag to be set
+		while(__HAL_I2S_GET_FLAG(&hi2s2, I2S_FLAG_RXP) != SET);
+
+		buffer[size - counter] = *((uint32_t *)&hi2s2.Instance->RXDR);
+
+		// check for overruns
+		if(__HAL_I2S_GET_FLAG(&hi2s2, I2S_FLAG_OVR) == SET)
+		{
+			// clear overrun flag
+			__HAL_I2S_CLEAR_OVRFLAG(&hi2s2);
+		}
+
+		counter--;
+	}
+
+	__HAL_I2S_DISABLE(&hi2s2);
 }
 
 void PortentaI2S::record(uint16_t* buffer, uint32_t size)
 {
-	
+	uint32_t counter = size, dummy;
+
+	// Check if I2S is enabled
+	if((hi2s2.Instance->CR1 & SPI_CR1_SPE) == SPI_CR1_SPE)
+	{
+		// Disable it
+		__HAL_I2S_DISABLE(&hi2s2);
+	}
+
+	// Clear I2S flags
+	__HAL_I2S_CLEAR_OVRFLAG(&hi2s2);
+	__HAL_I2S_CLEAR_TIFREFLAG(&hi2s2);
+	__HAL_I2S_CLEAR_UDRFLAG(&hi2s2);
+
+	// Check for dummy data in RXDR
+	if(__HAL_I2S_GET_FLAG(&hi2s2, I2S_FLAG_RXP) == SET)
+	{
+		dummy = hi2s2.Instance->RXDR;
+	}
+
+	// Enable I2S
+	__HAL_I2S_ENABLE(&hi2s2);
+
+	// Start data transfer
+	SET_BIT(hi2s2.Instance->CR1, SPI_CR1_CSTART);
+
+	while(counter > 0)
+	{
+		// wait for RXP flag to be set
+		while(__HAL_I2S_GET_FLAG(&hi2s2, I2S_FLAG_RXP) != SET);
+
+		buffer[size - counter] = *((uint16_t *)&hi2s2.Instance->RXDR);
+
+		// check for overruns
+		if(__HAL_I2S_GET_FLAG(&hi2s2, I2S_FLAG_OVR) == SET)
+		{
+			// clear overrun flag
+			__HAL_I2S_CLEAR_OVRFLAG(&hi2s2);
+		}
+
+		counter--;
+	}
+
+	__HAL_I2S_DISABLE(&hi2s2);
 }
